aa5419d613d231247c45fbb31371e3c562a1dfd5
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-07-07 08:17:30 +0530

    Add files via upload

---
+++ 05 Variable & Data Structure/Sum.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+
+class Sum {
+    public static void main(String args[])   {  
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+
+    }
+}
