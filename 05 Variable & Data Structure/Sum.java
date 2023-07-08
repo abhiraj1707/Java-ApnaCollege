20461823a9c7f6537aabdc7250e45c024faf6dab
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-07-08 21:39:11 +0530

    Sum.java

--- 05 Variable & Data Structure/Sum.java
+++ 05 Variable & Data Structure/Sum.java
@@ -1,3 +1,4 @@
+//Sun Of a & b
 import java.lang.*;
 import java.util.*;
 
@@ -8,5 +9,19 @@ class Sum {
         int sum = a + b;
         System.out.println(sum);
 
+    }
+}
+
+//Sum Of a & b (Input From User)
+import java.lang.*;
+import java.util.*;
+
+class Sum {
+    public static void main(String args[])  {
+         Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         int sum = a + b;
+         System.out.println(sum);
     }
 }
