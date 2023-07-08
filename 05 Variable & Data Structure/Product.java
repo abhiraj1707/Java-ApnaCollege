0024eb3c62fd534047d3bd99b233379693b192b6
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-07-08 22:01:23 +0530

    Product.java

---
+++ 05 Variable & Data Structure/Product.java
@@ -0,0 +1,13 @@
+//Product Of a & b (Input From User)
+import java.lang.*;
+import java.util.*;
+
+class Product {
+    public static void main(String args[])  {
+         Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         int product = a * b;
+         System.out.println(product);
+    }
+}
