2f07b885e0e6135e18ef0b7da78b1efcf7d6da0d
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-07-11 23:07:11 +0530

    Add files via upload

---
+++ 07 Conditional Statement/Pass_Fail.java
@@ -0,0 +1,8 @@
+public class Pass_Fail {
+    public static void main(String args[]) {
+        int marks = 95;
+
+        String reportCard = marks >=33 ? "PASS" : "FAIL";
+        System.out.println(reportCard);
+    }
+}
