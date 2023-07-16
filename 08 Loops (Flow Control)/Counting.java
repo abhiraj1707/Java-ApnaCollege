3f956348ccf2bf9ee4d820b7205bf69a8900d8ea
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-07-16 21:48:42 +0530

    Add files via upload

---
+++ 08 Loops (Flow Control)/Counting.java
@@ -0,0 +1,11 @@
+//Print Number From 1 To 10 using while loop.
+public class Counting {
+    public static void main (String[] args) {
+        int i = 1;
+        while(i <= 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
