fc69d0f64b8cfb0390d24a4089b875c479666bd4
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-07-16 10:41:12 +0530

    Add files via upload

---
+++ 08 Loops (Flow Control)/Hello.java
@@ -0,0 +1,12 @@
+//Hello World using While loop.
+public class Hello {
+    public static void main (String args[])
+    {
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Hello World");
+            i++;
+        }
+        System.out.println("Printed Hello World 5 time");
+    }
+}
