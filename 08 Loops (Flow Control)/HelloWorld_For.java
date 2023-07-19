2e6f1c19438cf7d5d4e5f4104549efca7201c122
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-07-19 20:58:37 +0530

    Add files via upload

---
+++ 08 Loops (Flow Control)/HelloWorld_For.java
@@ -0,0 +1,8 @@
+//Print Hello World using For Loop.
+public class HelloWorld_For {
+public static void main(String args[]) {
+    for(int i = 1; i <= 10; i++) {
+        System.out.println("Hello World");
+        }
+    }
+}
