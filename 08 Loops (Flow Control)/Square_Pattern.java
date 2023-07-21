c1374e074333d8c9ed7142f416a841bf2c5d07d8
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-07-21 20:37:48 +0530

    Add files via upload

---
+++ 08 Loops (Flow Control)/Square_Pattern.java
@@ -0,0 +1,20 @@
+public class Square_Pattern {
+    public static void main(String args[]) {
+        for(int i = 1; i <= 4; i++) {
+            System.out.println("****");
+        }
+    }
+}
+
+
+/* 
+ public class Square_Pattern {
+    public static void main(String args[]) {
+        int i = 1;
+        while (i <= 4) {
+           System.out.println("****"); 
+           i++;
+        }
+    }
+}
+*/
