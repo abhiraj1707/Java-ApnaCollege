a0180ba0a8fef4404c9d1f9dd70526aab655133a
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-07-22 21:34:57 +0530

    Add files via upload

---
+++ 08 Loops (Flow Control)/HW_Do_While.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class HW_Do_While {
+    public static void main (String args[]) {
+        int n = 1;
+        do {
+            System.out.println("Hello World");
+            n++;        
+        }
+        while ( n <= 10);
+    }
+}
