3606bca28e91b8865df339958dde6020ba3b13cf
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-07-31 20:28:38 +0530

    Add files via upload

---
+++ 10 Function & Methods/Swap.java
@@ -0,0 +1,17 @@
+//Program for swapping the value.
+public class Swap {
+    public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+    public static void main(String args[]) {
+
+        int a = 10;
+        int b = 15;
+        swap(a, b);
+    }
+}
