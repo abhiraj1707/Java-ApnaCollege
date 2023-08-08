978ce14bf8850b00fca83b7182a25e337ecf833b
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-08-08 23:07:42 +0530

    Product.java

--- 10 Function & Methods/Product.java
+++ 10 Function & Methods/Product.java
@@ -6,10 +6,13 @@ public class Product {
     }
     public static void main(String []args) {
          Scanner sc = new Scanner(System.in);
+        
          System.out.print("Enter the value of a : ");
          int a = sc.nextInt();
+        
          System.out.print("Enter the value of b : ");
          int b = sc.nextInt();
+        
          int prod = multiply(a, b);
         System.out.println("a * b = "+ prod);
     }
