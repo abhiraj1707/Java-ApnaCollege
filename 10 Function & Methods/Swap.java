f47c5e44627aae99a15b402ba93fef2e911790e3
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-08-19 21:38:54 +0530

    Add files via upload

---
+++ 10 Function & Methods/Swap.java
@@ -0,0 +1,25 @@
+//Program for swapping the value.
+
+import java.util.Scanner;
+
+public class Swap {
+    public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+    public static void main(String args[]) {
+        Scanner sc =  new Scanner(System.in);
+        
+        System.out.print("Enter the value of a : ");
+        int a = sc.nextInt();
+        
+        System.out.print("Enter the value of b : ");
+        int b = sc.nextInt();
+
+        swap(a, b);
+    }
+}
