041c029db95cccee123d07b6cda762e0c79d5666
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-08-28 07:10:26 +0530

    Add files via upload

---
+++ 11 Pattern Advanced (Part - II)/Hollow_Rhombus_Pattern.java
@@ -0,0 +1,37 @@
+/*
+            
+            ********
+           *      *
+          *      *
+         *      *
+        *      *
+       *      *
+      *      *
+     ********
+                        
+                        */
+
+public class Hollow_Rhombus_Pattern {
+    public static void hollow_rhombus(int n) {
+        for(int i=1; i<=n; i++) {
+            //Spaces
+            for(int j=1; j<=(n-i); j++) {
+                System.out.print(" ");
+            }
+
+            //hallow rectangle - star
+            for(int j=1; j<=n; j++) {
+                if(i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } 
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        hollow_rhombus(8);
+    }
+}
