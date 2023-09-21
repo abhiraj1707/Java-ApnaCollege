22722712e828539dc674770e945595052ad048db
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-09-21 21:54:19 +0530

    Add files via upload

---
+++ 15 Strings/String_Concatenation.java
@@ -0,0 +1,8 @@
+public class String_Concatenation {
+    public static void main(String args[]) {
+        String firstName = "Abhiraj";
+        String lastName = "Singh";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
