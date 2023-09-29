1da52c9b4024ac9b75d53b91bb04888787fe750e
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-09-29 21:37:21 +0530

    Add files via upload

---
+++ 15 Strings/Largest_String.java
@@ -0,0 +1,14 @@
+public class Largest_String {
+    public static void main(String args[]) {
+        String fruits[] = {"apple", "mango", "banana", "papaya"};
+
+        String largest  = fruits[0];
+        for(int i=0; i<fruits.length; i++) {
+            if(largest.compareTo(fruits[i]) < 0) {
+                largest = fruits[i];
+            }
+        }
+
+        System.out.println(largest);
+    }
+}
