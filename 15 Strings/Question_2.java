877200faeb50a74e7c83cc5e912941848c7f6fd7
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-10-04 19:06:48 +0530

    Add files via upload

---
+++ 15 Strings/Question_2.java
@@ -0,0 +1,11 @@
+//What will be the output of the following code?
+
+public class Question_2 {
+    public static void main(String args[]) {
+        String str = "ShradhaDidi";
+        String str1 = "ApnaCollege";
+        String str2 = "ShradhaDidi";
+        System.out.println(str.equals(str1) + " " + str.equals(str2));
+    }
+}
+//Output : false true
