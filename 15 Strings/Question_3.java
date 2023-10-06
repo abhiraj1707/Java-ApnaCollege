9ad741d8a714d6b3accf4e18a9dbba7e45e5995e
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-10-06 15:39:43 +0530

    Add files via upload

---
+++ 15 Strings/Question_3.java
@@ -0,0 +1,10 @@
+//What will be the output of the following code?
+
+public class Question_3 {
+    public static void main(String args[]) {
+        String str = "AbhirajSingh" .replace("1", "");
+        System.out.println(str);
+    }
+}
+
+//Output : AbhirajSingh
