677be6af920c69aab316b49a9cfac3eece584617
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-10-17 19:58:08 +0530

    Add files via upload

---
+++ 16 Bit Manipulation/Clear_ith_Bit.java
@@ -0,0 +1,12 @@
+public class Clear_ith_Bit {
+    public static int clearIthBit(int n, int i) {
+        int bitMask= ~(1<<i);
+        return n & bitMask;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(clearIthBit(10, 1));
+    }
+}
+
+//Output : 8
