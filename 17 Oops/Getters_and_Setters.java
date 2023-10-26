8f4081138317dc06fca193f7d9f4364c26e0db49
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-10-26 18:02:44 +0530

    Add files via upload

---
+++ 17 Oops/Getters_and_Setters.java
@@ -0,0 +1,35 @@
+public class Getters_and_Setters {
+    
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.println(p1.getColor());
+        p1.setTip(5);
+        System.out.println(p1.getTip());
+        p1.setColor("Yellow");
+        System.out.println(p1.getColor());
+        p1.setTip(9);
+        System.out.println(p1.getTip());
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    void setTip(int tip) {
+        this.tip = tip;
+    }
+}
