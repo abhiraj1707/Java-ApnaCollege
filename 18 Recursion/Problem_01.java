8344575794ee3aeceb3a67ec09fd8f75b73ed15f
Abhiraj Singh <118660683+abhiraj1707@users.noreply.github.com>
2023-11-21 21:16:15 +0530

    Add files via upload

---
+++ 18 Recursion/Problem_01.java
@@ -0,0 +1,17 @@
+/*
+Print numbers from n to 1 (Decreasing order)
+*/
+public class Problem_01 {
+    public static void printDec(int n) {
+        if(n == 1) {
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n+" ");
+        printDec(n-1);
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        printDec(n);
+    }
+}
